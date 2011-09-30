5432527d7cf3bf09661b48d726613b5d600c2089
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-09-30 12:05:29 -0430

    main.c: implemented getopt arguments with failure support. 	.gitignore: ignoring main and verific files.

---
+++ main.c
@@ -0,0 +1,82 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define DEFAULT_TIME 30
+#define EXIT_FAILURE 1
+
+int 
+usage (int status) 
+{
+  fprintf (stderr, 
+           "Uso: ./verific [-t <num_segundos>] [-d <directorio>] [-a <archivo.txt>] \n\
+   -t      intervalo de tiempo que pasa hasta que la aplicación verifica cambios en archivos. \n\
+   -d      directorio donde se desea verificar cambios en archivos. \n\
+   -a      archivo con lista de directorios a verificar. \n");
+  exit (status);
+}
+
+int 
+main (int argc, char **argv) 
+{
+  if (argc == 1)
+    usage (EXIT_FAILURE);
+
+  int a, index;
+  unsigned int time = -1;
+  char dir[42];
+  char *file = NULL;
+  FILE *fd = NULL;
+  
+  opterr = 0;
+
+  while ((a = getopt (argc, argv, "t:d:a:")) != -1) 
+    switch (a)
+      {
+      case 't':
+        if (optarg != 0)
+          time = atoi(optarg);
+        break;
+
+      case 'd':
+        strcpy (dir, optarg);
+        break;
+
+      case 'a':
+        file = optarg;
+        fd = fopen (file, "r");
+        break;
+
+      case '?':
+        if (optopt == 'a' || optopt == 'd' || optopt == 't')
+          fprintf (stderr, "La opción -%c requiere un argumento. \n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Opción desconocida `-%c'. \n", optopt);
+        else 
+          fprintf (stderr, "Caracter de opción desconocido `\\x%x'. \n", optopt);
+        usage (EXIT_FAILURE);
+        break;
+        
+      case ':':
+        fprintf (stderr, "Opción sin argumento. \n");
+        break;
+
+      default:
+        usage (EXIT_FAILURE);
+      }
+
+  if (time == -1)
+    time = DEFAULT_TIME; 
+
+  for (index = optind; index < argc; index++) {
+    printf ("%s no es una opción\n", argv[index]);
+    usage (EXIT_FAILURE);
+  }
+
+  if (fd) 
+      fclose (fd);   
+  
+  return 0;
+}
