ccacb616345b9ec1a412c9d709e4fed2d76ff235
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-10-10 15:27:13 -0430

    dispatcher.c: calling printer function.

--- dispatcher.c
+++ dispatcher.c
@@ -14,31 +14,7 @@ dispatcher(URL* url_list){
   printf("DIFFER DONE\n");
 
   updater(diffs);
-  printf("UPDATER DONE\n");
+  printer (diffs);
+  free_list (diffs);
+  return;
 }
-
-  /* int ret = 0; //to hold return values */
-  /* int i = 0; //iteration variable */
-
-  /* unsigned short naddrs = 1; */
-  /* struct addrinfo* addrs = (struct addrinfo*)malloc(sizeof(struct addrinfo)*naddrs); */
-  /* for(i = 0; i < naddrs; ++i){ */
-  /*   memset(&(addrs[i]),0,sizeof(struct addrinfo)); */
-  /* } */
-
-  /* //Initialize hints structure to create addrinfos */
-  /* struct addrinfo hints; */
-  /* memset(&hints,0,sizeof(struct addrinfo)); */
-  /* hints.ai_family = AF_INET; */
-  /* hints.ai_socktype = SOCK_STREAM; */
-  /* //The protocol is left to the program to decide. */
-  /* //Since there aren't any protocols that are actually */
-  /* //used for streaming besides TCP, we can leave it */
-  /* //at it's default value. The correct behaviour would */
-  /* //be to use 'getprotoent'. */
-
-  /* ret = getaddrinfo("159.90.9.202","http",&hints,&addrs);; */
-  /* if (ret){ */
-  /*   printf("Error getting info about host"); */
-  /*   exit(1); */
-  /* } */
