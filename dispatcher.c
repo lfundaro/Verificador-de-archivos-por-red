57c517cb2ea8fe3186aa435cc7f274dfa566c60a
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-10-11 19:15:07 -0430

    dispatcher.c: commented out flags.

--- dispatcher.c
+++ dispatcher.c
@@ -6,14 +6,17 @@ void
 dispatcher(URL* url_list){
   int nurls = 0;
 
+  //  printf("FETCHER START\n"); //(FLAG)
   char** pgs = fetcher(url_list,&nurls);
-  printf("FETCHER DONE\n");
+  //printf("FETCHER DONE\n"); //(FLAG)
 
+  //printf("PARSER START\n"); //(FLAG)
   entry_node* entries = parser(pgs,nurls);
-  printf("PARSER DONE\n");
+  //printf("PARSER DONE\n"); //(FLAG)
 
+  //  printf("DIFFER START\n"); //(FLAG)
   entry_node* diffs = differ(entries);
-  printf("DIFFER DONE\n");
+  //  printf("DIFFER DONE\n"); //(FLAG)
 
   updater(diffs);
   printer(diffs);
