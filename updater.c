0bb921a4194378e758a1567de903bc9a2f99c802
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-10-11 20:41:06 -0430

    updater.c: commented code.

--- updater.c
+++ updater.c
@@ -4,12 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+// Actualiza tabla de hash en caso de encontrar un archivo nuevo
+// o una modificación.
 void update(struct fileEntry e){
   if (e.dt == NODIFF)
     return;
   else 
     {
+      // Preparar estructura ENTRY para tabla de hash.
       ENTRY *p = (ENTRY *) malloc (sizeof(ENTRY));
       int keylength = strlen (e.URL) + strlen (e.path) + 1;
       char *key_str = (char *) malloc (sizeof (char) * keylength);
@@ -17,19 +19,23 @@ void update(struct fileEntry e){
       strcat (key_str, e.path);
       p->key = strdup (key_str);
       p->data = strdup (e.date);
-      if (e.dt == NEW) 
+      if (e.dt == NEW)  // NUEVA ENTRADA
         hsearch (*p, ENTER);
-      else // MODIFIY
+      else // MODIFICAR ENTRADA
         {
           ENTRY *pmodif = hsearch (*p, FIND);
           strcpy (pmodif->data, e.date);
         }
+      // Liberar recursos
+      free (p->data);
+      free (p->key);
       free (p);
       free (key_str);
     }
   return;
 }
 
+// Llama a función update
 void updater(entry_node* diffs){
   entry_node* curr = diffs;//Variable de iteracion
 
